eca531fcecc270787134e2ee9e1136f012d1f9a9
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-08 13:49:39 +0100

    Project gemaakt en bezig met flash screen

---
+++ app/src/main/java/com/example/bilalelayachi/whattodo/welkom.java
@@ -0,0 +1,26 @@
+package com.example.bilalelayachi.whattodo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class welkom extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_welkom);
+
+
+    }
+
+
+
+
+}
